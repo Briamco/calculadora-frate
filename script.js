c2d5d8b967be12ecafa1c22759764afaea48eccb
Briamco <crossinerobriamco@gmail.com>
2025-09-22 12:33:05 -0400

    Add initial HTML, CSS, and JavaScript files for the Fraternity Years Calculator

---
+++ script.js
@@ -0,0 +1,22 @@
+const year = new Date().getFullYear();
+const form = document.querySelector("form");
+const result = document.getElementById("result");
+const section = document.querySelector("section");
+const resetBtn = document.getElementById("resetBtn");
+
+section.hidden = true;
+
+resetBtn.addEventListener("click", () => {
+  section.hidden = true;
+  form.classList.remove("hidden");
+  form.yearInput.value = "";
+});
+
+form.addEventListener("submit", () => {
+  event.preventDefault();
+  const inputYear = form.yearInput.valueAsNumber;
+  const yearsInFraternity = year - inputYear;
+  form.classList.add("hidden");
+  section.hidden = false;
+  result.textContent = `Años en la fraternidad: ${yearsInFraternity}`;
+});
